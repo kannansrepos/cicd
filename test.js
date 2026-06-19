ee5d77950a713346d8a555d6a7e97e6b7282b4b9
Kannan S <kannan.netcore@gmail.com>
2026-06-19 21:37:42 +0530

    Created Error

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { createServer, get } from 'http';
 
-const app = express();
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
+{
   res.send('Hello World!');
 });
 
@@ -10,27 +10,35 @@ const server = createServer(app);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, () =>
+{
   console.log(`Server is running on port ${PORT}`);
 
-  get(`http://localhost:${PORT}`, (res) => {
+  get(`http://localhost:${PORT}`, (res) =>
+  {
     console.log(`GET request to / returned status code: ${res.statusCode}`);
-    if (res.statusCode === 200) {
+    if (res.statusCode === 200)
+    {
       console.log('Test passed: Server responded with 200 OK');
-      server.close(() => {
+      server.close(() =>
+      {
         console.log('Server closed after test');
       });
       process.exit(0); // Exit with success
-    } else {
+    } else
+    {
       console.error('Test failed: Server did not respond with 200 OK');
-      server.close(() => {
+      server.close(() =>
+      {
         console.log('Server closed after test');
       });
       process.exit(1); // Exit with failure
     }
-  }).on('error', (err) => {
+  }).on('error', (err) =>
+  {
     console.error('Test failed: Error making GET request to /', err);
-    server.close(() => {
+    server.close(() =>
+    {
       console.log('Server closed after test');
     });
     process.exit(1); // Exit with failure
